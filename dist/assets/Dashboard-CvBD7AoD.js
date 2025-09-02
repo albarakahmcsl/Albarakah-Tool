505ef1bbcd2218b337e22ee06eebf69986c1182b
albarakahmcsl <albarakahmcsl25@gmail.com>
2025-09-02 11:44:01 +0400

    Publish application

--- dist/assets/Dashboard-CvBD7AoD.js
+++ dist/assets/Dashboard-CvBD7AoD.js
@@ -1,4 +1,4 @@
-import{c as t,u,a as b,U as d,F as m,h as r,j as e,d as i,q as l}from"./index-9pW-t-sw.js";import{u as o}from"./useQuery-Bi1nLucc.js";/**
+import{c as t,u,a as b,U as d,F as m,h as r,j as e,d as i,q as l}from"./index-BUbYa6VJ.js";import{u as o}from"./useQuery-CVl2lB-0.js";/**
  * @license lucide-react v0.344.0 - ISC
  *
  * This source code is licensed under the ISC license.
--- dist/assets/square-pen-DWVIh0ff.js
+++ dist/assets/square-pen-DWVIh0ff.js
@@ -1,4 +1,4 @@
-import{c as a}from"./index-9pW-t-sw.js";/**
+import{c as a}from"./index-BUbYa6VJ.js";/**
  * @license lucide-react v0.344.0 - ISC
  *
  * This source code is licensed under the ISC license.

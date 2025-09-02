fd41fb47d6c723254569a034020fc7ad5f2a0836
albarakahmcsl <albarakahmcsl25@gmail.com>
2025-09-02 12:58:25 +0400

    Publish application

--- dist/assets/square-pen-BXBrY6W0.js
+++ dist/assets/square-pen-BXBrY6W0.js
@@ -1,4 +1,4 @@
-import{c as a}from"./index-BUbYa6VJ.js";/**
+import{c as a}from"./index-DuNJr4fv.js";/**
  * @license lucide-react v0.344.0 - ISC
  *
  * This source code is licensed under the ISC license.
